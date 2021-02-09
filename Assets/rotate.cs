fb9d7347f4df8c50e1c8e15fccd43ab31565b1c6
Mihir0106 <mjmihir9617@gmail.com>
2021-02-09 17:40:04 +0530

    Unity Package

---
+++ Assets/rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   StartCoroutine("Rotate");
+    }
+/*
+    IEnumerator Rotate()
+    {
+        Vector3 rotate = transform.rotation.eulerAngles;
+        rotate.y += 0.1f;
+
+        gameObject.GetComponent<Transform>().rotation.y =  rotate.y;
+    }*/
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0.5f, 0f, Space.Self); 
+    }
+}
