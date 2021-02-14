084ab3a7cd04a2c6cc3207e3b42d8eab4aa36e19
Mihir0106 <mjmihir9617@gmail.com>
2021-02-15 00:13:02 +0530

    Shader

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-     //   StartCoroutine("Rotate");
-    }
-/*
-    IEnumerator Rotate()
-    {
-        Vector3 rotate = transform.rotation.eulerAngles;
-        rotate.y += 0.1f;
+    public float ySpeed;
 
-        gameObject.GetComponent<Transform>().rotation.y =  rotate.y;
-    }*/
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0f, 0.5f, 0f, Space.Self); 
+        transform.Rotate(0f, ySpeed, 0f, Space.Self); 
     }
 }
